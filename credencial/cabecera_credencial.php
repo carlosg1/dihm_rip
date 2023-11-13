db7b4eb5181f2e10714d2f3158e545fdc9d39fbb
Carlos Garcia <carlosgctes@gmail.com>
2023-11-13 09:42:17 -0300

    Generando pagina de credenciales

---
+++ credencial/cabecera_credencial.php
@@ -0,0 +1,7 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <?php include_once "../../fecha.php"; ?>
+        </div>
+    </div>
+</div>
---
+++ credencial/dolores_centurion/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Direcci&oacute;n de Industria, H. y M.</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+</head>
+<body>
+    <?php include_once "../cabecera_credencial.php"; ?>
+
+    <div class="row">
+        <h1>Personal t&eacute;cnico de la</h1>
+        <h1>Direcci&oacute;n de Industria, Hidrocarburos y Minería</h1>
+        <br/><br/>
+        <h2>Estamos actualizando los datos del equipo técnico...</h2>
+    </div>
+
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
+
